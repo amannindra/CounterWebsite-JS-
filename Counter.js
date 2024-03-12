cdc5e06feeb40f162d2f0275037665951226d069
Aman Nindra <amannindra@gmail.com>
2024-03-12 00:40:59 -0700

    commit

---
+++ Counter.js
@@ -0,0 +1,13 @@
+console.log("Check");
+let count = 0;
+
+const value = document.querySelector("#Value");
+const btns = document.querySelectorAll(".Gen");
+
+console.log(btns);
+
+btns.forEach(function (Gen) {
+  Gen.addEventListener("click",function(e){
+    
+  })
+});
